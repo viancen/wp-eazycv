f0d90741005f01e582cb432eebb020a3aeccc8fc
Vincent <vincent@inforvision.nl>
2019-05-24 15:06:42 +0200

    ping

--- vendor/viancen/eazycv-client/src/EazycvClient.php
+++ vendor/viancen/eazycv-client/src/EazycvClient.php
@@ -4,8 +4,7 @@ use GuzzleHttp\Client;
 
 require_once 'EazycvClient/Exceptions.php';
 
-class EazycvClient
-{
+class EazycvClient {
 	//private client (Guzzle)
 	private $client;
 
@@ -14,6 +13,7 @@ class EazycvClient
 
 	//API credentials
 	public $apiKey;
+
 	public $userKey = null;
 
 	//What part of Eazycv is used
@@ -38,45 +38,72 @@ class EazycvClient
 	 * @param null $root
 	 * @param array $options
 	 */
-	public function __construct($apikey = '', $customer = null, $root = null, $options = [])
-	{
-		if (!$apikey) throw new \Eazycv_Error('You must provide a Eazycv API key');
-		if (!$customer) throw new \Eazycv_Error('You must provide a Eazycv customer slug');
-		if (!$root) {
+	public function __construct( $apikey = '', $customer = null, $root = null, $options = [] ) {
+		if ( ! $apikey ) {
+			throw new \Eazycv_Error( 'You must provide a Eazycv API key' );
+		}
+		if ( ! $customer ) {
+			throw new \Eazycv_Error( 'You must provide a Eazycv customer slug' );
+		}
+		if ( ! $root ) {
 			$root = 'https://api.eazycv.cloud';
 		}
 
-		$this->apiKey = $apikey;
-		$this->root = $root;
+		$this->apiKey   = $apikey;
+		$this->root     = $root;
 		$this->customer = $customer;
 
-		if (!empty($options)) {
-			if (!empty($options['settings'])) {
+		if ( ! empty( $options ) ) {
+			if ( ! empty( $options['settings'] ) ) {
 				$this->settings = $options['settings'];
 			}
 		}
 
 		$this->client = new Client();
+		if ( ! $this->pingEazyCvServer() ) {
+			throw new \Eazycv_Error( 'EazyCV cannot be reached' );
+		}
+		$this->root = rtrim( $this->root, '/' ) . '/';
 
-		$this->root = rtrim($this->root, '/') . '/';
+	}
 
+	// Function to check response time
+	private function pingEazyCvServer() {
+		if ( strstr( $this->root, '.local' ) ) {
+			return true;
+		}
+		$starttime = microtime( true );
+		$file      = fsockopen( str_replace( [ 'http://', 'https://' ], '', $this->root ), 443, $errno, $errstr, 10 );
+		$stoptime  = microtime( true );
+		$status    = 0;
+
+		if ( ! $file ) {
+			$status = - 1;
+		}  // Site is down
+		else {
+			fclose( $file );
+			$status = ( $stoptime - $starttime ) * 1000;
+			$status = floor( $status );
+		}
+
+		return $status;
 	}
 
 	/**
 	 * Sets the token for the session of a logged in user
+	 *
 	 * @param $userKey
 	 */
-	public function setUserKey($userKey)
-	{
+	public function setUserKey( $userKey ) {
 		$this->userKey = $userKey;
 	}
 
 	/**
 	 * Gets all settings from eazycv
+	 *
 	 * @param $userKey
 	 */
-	public function getSettings()
-	{
+	public function getSettings() {
 		return $this->settings;
 	}
 
@@ -86,25 +113,30 @@ class EazycvClient
 	 * @param null $email
 	 * @param null $passWord
 	 * @param null $persistant
-	 * @throws Eazycv_Error
+	 *
 	 * @return array $sessionTokenToReplaceApiTokenWith
+	 * @throws Eazycv_Error
 	 */
-	public function loginUser($email = null, $passWord = null, $persistant = false)
-	{
-		if (!$email) throw new Eazycv_Error('You must provide a emailaddress');
-		if (!$passWord) throw new Eazycv_Error('You must provide a password');
+	public function loginUser( $email = null, $passWord = null, $persistant = false ) {
+		if ( ! $email ) {
+			throw new Eazycv_Error( 'You must provide a emailaddress' );
+		}
+		if ( ! $passWord ) {
+			throw new Eazycv_Error( 'You must provide a password' );
+		}
 
-		$data = $this->post('users/login', [
-			'email' => $email,
-			'password' => $passWord,
+		$data = $this->post( 'users/login', [
+			'email'      => $email,
+			'password'   => $passWord,
 			'persistant' => $persistant
-		]);
+		] );
 
-		if (!empty($data['session'])) {
-			$this->setUserKey($data['session']['token']);
+		if ( ! empty( $data['session'] ) ) {
+			$this->setUserKey( $data['session']['token'] );
+
 			return $data['session'];
 		} else {
-			throw new Eazycv_Error('Invalid credentials');
+			throw new Eazycv_Error( 'Invalid credentials' );
 		}
 
 	}
@@ -117,25 +149,30 @@ class EazycvClient
 	 * @param null $email
 	 * @param null $passWord
 	 * @param null $persistant
-	 * @throws Eazycv_Error
+	 *
 	 * @return array $sessionTokenToReplaceApiTokenWith
+	 * @throws Eazycv_Error
 	 */
-	public function loginCandidate($email = null, $passWord = null, $persistant = false)
-	{
-		if (!$email) throw new Eazycv_Error('You must provide a emailaddress');
-		if (!$passWord) throw new Eazycv_Error('You must provide a password');
+	public function loginCandidate( $email = null, $passWord = null, $persistant = false ) {
+		if ( ! $email ) {
+			throw new Eazycv_Error( 'You must provide a emailaddress' );
+		}
+		if ( ! $passWord ) {
+			throw new Eazycv_Error( 'You must provide a password' );
+		}
 
-		$data = $this->post('candidates/login', [
-			'email' => $email,
-			'password' => $passWord,
+		$data = $this->post( 'candidates/login', [
+			'email'      => $email,
+			'password'   => $passWord,
 			'persistant' => $persistant
-		]);
+		] );
 
-		if (!empty($data['session'])) {
+		if ( ! empty( $data['session'] ) ) {
 			$this->apiKey = $data['session']['token'];
+
 			return $data['session'];
 		} else {
-			throw new Eazycv_Error('Invalid credentials');
+			throw new Eazycv_Error( 'Invalid credentials' );
 		}
 
 	}
@@ -145,33 +182,34 @@ class EazycvClient
 	 *
 	 * @param $endpoint
 	 * @param array $params
+	 *
 	 * @return mixed
 	 */
-	public function post($endpoint, $params = [])
-	{
+	public function post( $endpoint, $params = [] ) {
 
 		try {
-			$response = $this->client->request('POST', $this->root . $endpoint, [
-				'headers' => [
-					'X-User' => $this->userKey,
+			$response = $this->client->request( 'POST', $this->root . $endpoint, [
+				'headers'        => [
+					'X-User'          => $this->userKey,
 					'X-Authorization' => $this->apiKey,
-					'X-Customer' => $this->customer,
+					'X-Customer'      => $this->customer,
 					'X-response-type' => 'json',
-					'Content-Type' => 'application/json',
+					'Content-Type'    => 'application/json',
 				],
 				'decode_content' => true,
 
-				'body' => json_encode($params)
-			]);
+				'body' => json_encode( $params )
+			] );
 
-		} catch (Eazycv_HttpError $error) {
+		} catch ( Eazycv_HttpError $error ) {
 			return [
-				'code' => $error->getCode(),
+				'code'    => $error->getCode(),
 				'message' => $error->getMessage()
 			];
 		}
 
-		$body = json_decode($response->getBody(), true);
+		$body = json_decode( $response->getBody(), true );
+
 		return $body;
 	}
 
@@ -179,33 +217,34 @@ class EazycvClient
 	 * Post request to EazyCV
 	 *
 	 * @param $endpoint
+	 *
 	 * @return mixed
 	 */
-	public function get($endpoint, $parameters = [])
-	{
+	public function get( $endpoint, $parameters = [] ) {
 
 		try {
-			$response = $this->client->request('GET', $this->root . $endpoint, [
-				'headers' => [
-					'X-User' => $this->userKey,
+			$response = $this->client->request( 'GET', $this->root . $endpoint, [
+				'headers'        => [
+					'X-User'          => $this->userKey,
 					'X-Authorization' => $this->apiKey,
-					'X-Customer' => $this->customer,
+					'X-Customer'      => $this->customer,
 					'X-response-type' => 'json',
-					'Content-Type' => 'application/json',
+					'Content-Type'    => 'application/json',
 				],
 				'decode_content' => true,
-				'verify' => false,
-				'query' => $parameters
-			]);
+				'verify'         => false,
+				'query'          => $parameters
+			] );
 
-		} catch (Eazycv_HttpError $error) {
+		} catch ( Eazycv_HttpError $error ) {
 			return [
-				'code' => $error->getCode(),
+				'code'    => $error->getCode(),
 				'message' => $error->getMessage()
 			];
 		}
 
-		$body = json_decode($response->getBody(), true);
+		$body = json_decode( $response->getBody(), true );
+
 		return $body;
 	}
 
@@ -213,33 +252,34 @@ class EazycvClient
 	 * Put request to Eazycv.io
 	 *
 	 * @param $endpoint
+	 *
 	 * @return mixed
 	 */
-	public function put($endpoint, $params)
-	{
+	public function put( $endpoint, $params ) {
 
 		try {
-			$response = $this->client->request('PUT', $this->root . $endpoint, [
-				'headers' => [
-					'X-User' => $this->userKey,
+			$response = $this->client->request( 'PUT', $this->root . $endpoint, [
+				'headers'        => [
+					'X-User'          => $this->userKey,
 					'X-Authorization' => $this->apiKey,
-					'X-Customer' => $this->customer,
+					'X-Customer'      => $this->customer,
 					'X-response-type' => 'json',
-					'Content-Type' => 'application/json',
+					'Content-Type'    => 'application/json',
 				],
 				'decode_content' => true,
-				'verify' => false,
-				'body' => json_encode($params)
-			]);
+				'verify'         => false,
+				'body'           => json_encode( $params )
+			] );
 
-		} catch (Eazycv_HttpError $error) {
+		} catch ( Eazycv_HttpError $error ) {
 			return [
-				'code' => $error->getCode(),
+				'code'    => $error->getCode(),
 				'message' => $error->getMessage()
 			];
 		}
 
-		$body = json_decode($response->getBody(), true);
+		$body = json_decode( $response->getBody(), true );
+
 		return $body;
 	}
 
@@ -247,32 +287,33 @@ class EazycvClient
 	 * Delete request
 	 *
 	 * @param $endpoint
+	 *
 	 * @return mixed
 	 */
-	public function del($endpoint)
-	{
+	public function del( $endpoint ) {
 
 		try {
-			$response = $this->client->request('DELETE', $this->root . $endpoint, [
-				'headers' => [
-					'X-User' => $this->userKey,
+			$response = $this->client->request( 'DELETE', $this->root . $endpoint, [
+				'headers'        => [
+					'X-User'          => $this->userKey,
 					'X-Authorization' => $this->apiKey,
-					'X-Customer' => $this->customer,
+					'X-Customer'      => $this->customer,
 					'X-response-type' => 'json',
-					'Content-Type' => 'application/json',
+					'Content-Type'    => 'application/json',
 				],
 				'decode_content' => true,
-				'verify' => false
-			]);
+				'verify'         => false
+			] );
 
-		} catch (Eazycv_HttpError $error) {
+		} catch ( Eazycv_HttpError $error ) {
 			return [
-				'code' => $error->getCode(),
+				'code'    => $error->getCode(),
 				'message' => $error->getMessage()
 			];
 		}
 
-		$body = json_decode($response->getBody(), true);
+		$body = json_decode( $response->getBody(), true );
+
 		return $body;
 	}
 
